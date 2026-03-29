ef868345bf5402be12b0b3609f9cc26689f64bca
AndreaMagri70 <assomag@libero.it>
2026-03-29 18:38:40 +0200

    Fix:ottimizzato Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="flex flex-col items-center justify-center text-center min-h-[70vh]">
+      <h1 className="text-3xl md:text-4xl font-semibold text-gray-900">
+        Welcome to MedCare
+      </h1>
+
+      <p className="mt-3 text-gray-500 text-sm md:text-base">
+        Your health, our priority.
+      </p>
+    </div>
   );
 }
--- components/convexclientprovider.tsx
+++ components/convexclientprovider.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useAuth } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ConvexReactClient } from "convex/react";
+import { Authenticated, ConvexReactClient, useMutation } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import React from "react";
+import React, { useEffect } from "react";
+
+import { api } from "@/convex/_generated/api";
 
 export default function ConvexClientProvider({ children }: { children: React.ReactNode }) {
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
@@ -12,9 +14,30 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 return (    
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABILE_KEY!}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        {children}
+        <Authenticated>
+          <UserSync />
+        </Authenticated>
+          {children}        
+
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
 }
+
+function UserSync() {
+  const { user } = useUser();
+  const createUser = useMutation(api.patients.createUser);
+
+  useEffect(() => {
+    if (user) {
+      createUser({
+        name: user.fullName || user.firstName || "Anonymous",
+        email: user.emailAddresses[0].emailAddress,
+      })
+    }
+  }, [user, createUser])
+      return null;
+  }
+
+
 
