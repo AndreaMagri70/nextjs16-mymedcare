9abf7401b4d4209aeb87b563b6833a5c85e756b0
AndreaMagri70 <assomag@libero.it>
2026-03-30 18:33:29 +0200

    Fix: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
+import Categories from "@/components/Categories";
+import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+import TopDoctors from "@/components/TopDoctors";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center text-center min-h-[70vh]">
-      <h1 className="text-3xl md:text-4xl font-semibold text-gray-900">
-        Welcome to MedCare
-      </h1>
-
-      <p className="mt-3 text-gray-500 text-sm md:text-base">
-        Your health, our priority.
-      </p>
+    <div className="flex flex-col min-h-screen">
+      <main className="grow">
+        <Hero />
+        <Stats />
+        <Categories />
+        <TopDoctors />
+      </main>
     </div>
   );
 }
