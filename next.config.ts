74f215d2aede1e811fc4ae58ff4f504ac6e909e2
AndreaMagri70 <assomag@libero.it>
2026-04-03 11:20:59 +0200

    Top

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "tailwind-generator.b-cdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
